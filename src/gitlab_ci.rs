567ab5fd1b57fe0b2dc4d46da1bd0d3da77077f6
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2022-03-26 11:36:58 +0100

    refactor

--- src/gitlab_ci.rs
+++ src/gitlab_ci.rs
@@ -1,31 +1,30 @@
-use regex::Regex;
+use regex::{Captures, Regex};
 use std::str::Split;
 use serde_yaml::Value;
 
-pub fn get_matrix (pipeline: &str, path_matrix: &str) -> Vec<String> {
-    let deserialized_map: Value = serde_yaml::from_str(&pipeline).unwrap();
-    let path_iterator : Split<char> = path_matrix.split('.');
+fn match_if_array_step (step: &str) -> Option<Captures> {
+    let regex_array = Regex::new(r"(?P<field>[a-zA-Z_]+)\[(?P<index>\d+)\]$").unwrap();
+    regex_array.captures(step)
+}
 
-    let mut current_value: &Value = &deserialized_map;
-    for step in path_iterator {
-        let rg_w_named = Regex::new(r"(?P<field>[a-zA-Z_]+)\[(?P<index>\d+)\]$").unwrap();
-        let matched = rg_w_named.captures(step);
-        if matched.is_none() {
-            current_value = current_value.get(step).unwrap();
-            continue
-        }
-        let matched = matched.unwrap();
-        let index = matched.name("index").unwrap().as_str();
-        let index :usize = index.parse().unwrap();
-        let field_name = matched.name("field").unwrap().as_str();
-        let array = current_value.get(field_name).unwrap();
-        let sequence = array.as_sequence().unwrap();
-        current_value = sequence.get(index).unwrap();
-    }
-    
+fn get_field_and_index (matched: Option<Captures>) -> (&str, usize) {
+    let matched = matched.unwrap();
+    let index = matched.name("index").unwrap().as_str();
+    let index :usize = index.parse().unwrap();
+    let field_name = matched.name("field").unwrap().as_str();
+    (field_name, index)
+}
+
+fn step_into_array <'a>(current: &'a Value, field_name: &str, index: usize) -> &'a Value {
+    let array = current.get(field_name).unwrap();
+    let sequence = array.as_sequence().unwrap();
+    sequence.get(index).unwrap()
+}
+
+fn get_list_of_version_from_matrix (matrix_value: &Value) -> Vec<String>{
     let mut vec_string : Vec<String> = Vec::new();
 
-    for item in current_value.as_sequence().unwrap().to_vec() {
+    for item in matrix_value.as_sequence().unwrap().to_vec() {
         let version = item.as_str().unwrap();
         let version = match Regex::new(r"^(\w+:)?(?P<version>[\d\.]+)$")
         .unwrap()
@@ -35,6 +34,25 @@ pub fn get_matrix (pipeline: &str, path_matrix: &str) -> Vec<String> {
         };
         vec_string.push(String::from(version));
     }
+    vec_string
+}
+
+pub fn get_matrix (pipeline: &str, path_matrix: &str) -> Vec<String> {
+    let deserialized_map: Value = serde_yaml::from_str(&pipeline).unwrap();
+    let path_iterator : Split<char> = path_matrix.split('.');
+
+    let mut current_value: &Value = &deserialized_map;
+    for step in path_iterator {
+        let matched = match_if_array_step(step);
+        if matched.is_none() {
+            current_value = current_value.get(step).unwrap();
+            continue
+        }
+        let (field_name, index) = get_field_and_index(matched);
+        current_value = step_into_array(current_value, &field_name, index);
+    }
+    
+    let vec_string : Vec<String> = get_list_of_version_from_matrix(current_value);
    
     vec_string
 }
